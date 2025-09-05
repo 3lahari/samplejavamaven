cde139d9c82fb9a5db08b266e666630f86bbe247
lahari <lahariguttula12355@gmail.com>
2025-09-05 22:00:29 +0530

    sample java maven

---
+++ src/main/java/com/kmit/samplejavamaven/App.java
@@ -0,0 +1,13 @@
+package com.kmit.samplejavamaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hey buddy!" );
+    }
+}
